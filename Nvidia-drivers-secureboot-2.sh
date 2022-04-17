898c47c395b6dfd78b414eb7ba532c8914d63d75
Eowii <andreas.ljunggren93@gmail.com>
2022-04-17 02:30:01 +0200

    Update Nvidia-drivers-secureboot-2.sh

--- Nvidia-drivers-secureboot-2.sh
+++ Nvidia-drivers-secureboot-2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Enable 2 COPRs
-sudo dnf copr enable egeretto/kmodtool-secureboot
-sudo dnf copr enable egeretto/akmods-secureboot
+sudo dnf copr enable egeretto/kmodtool-secureboot -y
+sudo dnf copr enable egeretto/akmods-secureboot -y
 sudo dnf update --refresh
 
 # Install the modified versions of kmodtool and akmods
-sudo dnf install kmodtool akmods
+sudo dnf install kmodtool akmods -y
 
 #Move the public and private parts of the key to the akmods dir and set secure permissions
 sudo mv ~/driver-signing.der /etc/pki/akmods/certs/public_key.der
@@ -18,7 +18,7 @@ sudo chmod 640 /etc/pki/akmods/private/private_key.priv
 
 
 # Install NVidia driver
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 -y
 
 # Make sure the kernel modules got compiled
 sudo akmods --force
