d2503b3c0478a7c1b211de883ba7da9375b4d6d6
Eowii <80362323+Eowii@users.noreply.github.com>
2022-04-23 17:16:38 +0200

    123123

--- dotfiles/polybar/polybar-scripts/cal-popup.sh
+++ dotfiles/polybar/polybar-scripts/cal-popup.sh
@@ -17,9 +17,11 @@ case "$1" in
 
     # X
     if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
-        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
-    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
+        #: $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
         : $((pos_x = BORDER_SIZE))
+    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
+        #: $((pos_x = BORDER_SIZE))
+        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE + 200))
     else #Center
         : $((pos_x = X - YAD_WIDTH / 2))
     fi
@@ -28,10 +30,10 @@ case "$1" in
     if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
         : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
     else #Top
-        : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
+        : $((pos_y = BAR_HEIGHT + BORDER_SIZE - 20))
     fi
 
-    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons --show-weeks\
         --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
         --title="yad-calendar" --borders=0 >/dev/null &
     ;;
