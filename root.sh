323e07ee0a026dc303e6623788faacaf087929ef
Eowii <80362323+Eowii@users.noreply.github.com>
2022-04-23 17:22:54 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -10,7 +10,7 @@ dnf upgrade --refresh -y
 
 # Add base packages
 
-dnf install rofi unzip picom bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lxpolkit lxappearance feh xsetroot dunst nano xset -y
+dnf install rofi yad unzip picom bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lxpolkit lxappearance feh xsetroot dunst nano xset -y
 dnf install @base-x @firefox -y
 dnf install papirus-icon-theme google-noto-emoji-color-fonts fira-code-fonts fontawesome5-fonts-all qt5-qtsvg qt5-qtquickcontrols arandr pavucontrol -y
 dnf install -y autoconf automake cairo-devel fontconfig gcc libev-devel libjpeg-turbo-devel libXinerama libxkbcommon-devel libxkbcommon-x11-devel libXrandr pam-devel pkgconf xcb-util-image-devel xcb-util-xrm-devel -y
