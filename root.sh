21b9025d0fa4ba71b1c615b2da28c2257a78bbf8
Eowii <80362323+Eowii@users.noreply.github.com>
2022-06-28 21:02:38 +0200

    123

--- root.sh
+++ root.sh
@@ -53,14 +53,17 @@ dnf install GitHubDesktop-linux-3.0.0-linux2.rpm -y
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
+#Installing flatpaks
+flatpak install $(cat GitHub/Fedora/install-flatpak.txt) -y
+
 #Parsec
-flatpak install parsec -y
+#flatpak install parsec -y
 
 #Discord
-flatpak install discord -y
+#flatpak install discord -y
 
 #Spotify
-flatpak install spotify -y
+#flatpak install spotify -y
 
 #BitWarden
 mkdir -p /home/$username/AppImg
