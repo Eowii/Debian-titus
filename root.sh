283c730b4e773a6cc8cb0aa1efd5d84ddb162f42
Eowii <80362323+Eowii@users.noreply.github.com>
2022-07-02 17:00:58 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -165,6 +165,8 @@ cp user.sh /home/$username/
 
 echo "bash ~/user.sh" >> /home/$username/.config/bspwm/bspwmrc
 
+ln -sf /home/$username/GitHub/Fedora/user.js /home/$username/.mozilla/firefox/*.default-release/
+
 chown -R $username:$username /home/$username
 
 # Boots into graphical UI
