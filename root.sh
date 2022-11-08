15b97dff32b6ba5a888f6c44db652195cd297bea
Eowii <andreas.ljunggren93@gmail.com>
2022-11-08 15:10:05 +0100

    123

--- root.sh
+++ root.sh
@@ -10,14 +10,14 @@ echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf copr enable gloriouseggroll/nobara -y
-dnf copr enable gloriouseggroll/mesa-aco -y
-dnf copr enable gloriouseggroll/glibc -y
-dnf copr enable gloriouseggroll/game-utils -y
-dnf copr enable gloriouseggroll/openal-soft -y
-dnf copr enable gloriouseggroll/edk2 -y
-dnf copr enable gloriouseggroll/obs-studio-gamecapture -y
-dnf copr enable sentry/kernel-fsync -y
-dnf copr enable sentry/xone -y
+#dnf copr enable gloriouseggroll/mesa-aco -y
+#dnf copr enable gloriouseggroll/glibc -y
+#dnf copr enable gloriouseggroll/game-utils -y
+#dnf copr enable gloriouseggroll/openal-soft -y
+#dnf copr enable gloriouseggroll/edk2 -y
+#dnf copr enable gloriouseggroll/obs-studio-gamecapture -y
+#dnf copr enable sentry/kernel-fsync -y
+#dnf copr enable sentry/xone -y
 
 #VSCodium
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
