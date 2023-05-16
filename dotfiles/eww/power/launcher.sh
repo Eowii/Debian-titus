bd4c6b686b8baeaedac830c55bcfe967a975dcf2
Eowii <80362323+Eowii@users.noreply.github.com>
2023-05-16 16:05:24 +0200

    123

--- dotfiles/eww/power/launcher.sh
+++ dotfiles/eww/power/launcher.sh
@@ -1,7 +1,7 @@
 cd "$(head -1 ~/.config/eww/power/directory.txt)"
 
-./eww open poweroff
-./eww open restart
+eww open poweroff
+eww open restart
 #./eww open log_out
-./eww open lock
-./eww open close
+eww open lock
+eww open close
