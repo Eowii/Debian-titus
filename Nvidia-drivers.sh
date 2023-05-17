b03adcca240e73a44090b8bb3c2a243c2d2d067a
Eowii <80362323+Eowii@users.noreply.github.com>
2023-05-17 14:13:05 +0200

    Nvidia driver fix

---
+++ Nvidia-drivers.sh
@@ -0,0 +1,3 @@
+# Install NVidia driver
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda -y
+
